a0c9ac3f0105884da3a8d5dfb338bc1fc80f28e2
William Freeman <wfreeman@fmgl.com.au>
2021-11-24 21:34:33 +0800

    initial commit

---
+++ .aws-sam/build/LambdaRead/resumeVisitorCount.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table("appData")
+
+def get_count():
+    resp = table.get_item(
+    Key={
+        'id':'resumechallengevisitors'
+    }
+)
+    return int(resp['Item']['count'])
+
+def lambda_handler(event, context):
+    old_count = get_count()
+    print("old count was "+str(old_count))
+    new_count = old_count + 1
+    print("new count is "+str(new_count))
+
+    table.put_item(
+       Item={
+            'id':'resumechallengevisitors',
+            'count': new_count
+        }
+    )
+    
+    return {  
+        'statusCode': 200,
+        'body': json.dumps(new_count)
+    }
