c45726ef3e8b52a1a387f0364e54c96f3384b2ba
William Freeman <wfreeman@fmgl.com.au>
2021-11-25 18:41:05 +0800

    fixed cors lambda

--- visitorCount/resumeVisitorCount.py
+++ visitorCount/resumeVisitorCount.py
@@ -27,5 +27,8 @@ def lambda_handler(event, context):
     
     return {  
         'statusCode': 200,
+        'headers' : {
+            "Access-Control-Allow-Origin" : "*"
+    },
         'body': json.dumps(new_count)
     }
